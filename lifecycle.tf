6fcc1a900161b3607531923cedc83a0e408adf63
Airaj24 <rajasekarellappan24@gmail.com>
2025-12-20 21:48:38 +0530

    CICD yaml file added

--- lifecycle.tf
+++ lifecycle.tf
@@ -1,10 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami               = "ami-0f1dcc636b69a6438" # Amazon Linux
+  ami               = "ami-068c0051b15cdb816" # Amazon Linux
   instance_type     = "t2.micro"
   # availability_zone = "ap-south-1a"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1a"
   tags = {
     "Name" = "web-3"
   }
