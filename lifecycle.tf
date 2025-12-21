bd68c2a0ba443fb9960b5f70d3be33068420f6fe
rajasekar <rajasekarellappan24@gmail.com>
2025-12-21 16:52:18 +0530

    ec2 added

--- lifecycle.tf
+++ lifecycle.tf
@@ -26,3 +26,18 @@ resource "aws_instance" "web" {
     ignore_changes = [tags]
   }
 }
+
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami               = "ami-068c0051b15cdb816" # Amazon Linux
+  instance_type     = "t2.micro"
+  # availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
+  tags = {
+    "Name" = "raja"
+  }
+  lifecycle {
+   #ignore_changes = [ami, availability_zone]
+    ignore_changes = [tags]
+  }
+}
